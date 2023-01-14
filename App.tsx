72f51038a96615207b522fe47b697ba9611ee21e
OscarDRT <55116114+OscarDRT@users.noreply.github.com>
2023-01-13 20:47:01 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { Navigation } from '@root/navigation'
+
+import { ThemeProvider } from '@root/theme/ThemeProvider'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <SafeAreaProvider>
+        <Navigation />
+      </SafeAreaProvider>
+    </ThemeProvider>
+  )
+}
